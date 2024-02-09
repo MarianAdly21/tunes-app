062232beeea7b72bdada25263fcb93a6c860c483
Marian Adly <marianadly2004@gmail.com>
2024-02-09 15:46:15 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TunesApp());
+}
+
+class TunesApp extends StatelessWidget {
+  const TunesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(),
+    );
+  }
+}
